939500d8e30e82f59da2b46697c0621b23d6f29d
Rahim Uddin Rafin <rahim.rafin.1@gmail.com>
2023-05-25 14:04:20 +0600

    problem-1-solved

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,6 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Problem1 = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    status: "",
+  });
+
+  const [allData, setAllData] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setAllData([...allData, formData]);
+  };
+
   const [show, setShow] = useState("all");
 
   const handleClick = (val) => {
@@ -12,12 +24,25 @@ const Problem1 = () => {
       <div className="row justify-content-center mt-5">
         <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
         <div className="col-6 ">
-          <form className="row gy-2 gx-3 align-items-center mb-4">
+          <form
+            onSubmit={(e) => handleSubmit(e)}
+            className="row gy-2 gx-3 align-items-center mb-4"
+          >
             <div className="col-auto">
-              <input type="text" className="form-control" placeholder="Name" />
+              <input
+                onBlur={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
+                type="text"
+                className="form-control"
+                placeholder="Name"
+              />
             </div>
             <div className="col-auto">
               <input
+                onBlur={(e) =>
+                  setFormData({ ...formData, status: e.target.value })
+                }
                 type="text"
                 className="form-control"
                 placeholder="Status"
@@ -68,7 +93,60 @@ const Problem1 = () => {
                 <th scope="col">Status</th>
               </tr>
             </thead>
-            <tbody></tbody>
+            {show === "all" &&
+              allData
+                ?.sort(function (a, b) {
+                  if (
+                    a.status.toLowerCase() === "active" &&
+                    b.status.toLowerCase() !== "active"
+                  ) {
+                    return -1;
+                  } else if (
+                    a.status.toLowerCase() === "completed" &&
+                    b.status.toLowerCase() !== "active"
+                  ) {
+                    return -1;
+                  }
+                  if (a.status > b.status) {
+                    return 1;
+                  }
+                })
+                .map((dt, id) => {
+                  return (
+                    <tbody key={id}>
+                      <tr>
+                        <td>{dt?.name}</td>
+                        <td>{dt?.status}</td>
+                      </tr>
+                    </tbody>
+                  );
+                })}
+            {show === "active" &&
+              allData
+                ?.filter((data) => data?.status.toLowerCase() === "active")
+                .map((dt, id) => {
+                  return (
+                    <tbody key={id}>
+                      <tr>
+                        <td>{dt?.name}</td>
+                        <td>{dt?.status}</td>
+                      </tr>
+                    </tbody>
+                  );
+                })}
+            {show === "completed" &&
+              allData
+                ?.filter((data) => data?.status.toLowerCase() === "completed")
+                .map((dt, id) => {
+                  return (
+                    <tbody key={id}>
+                      <tr>
+                        <td>{dt?.name}</td>
+                        <td>{dt?.status}</td>
+                      </tr>
+                    </tbody>
+                  );
+                })}
           </table>
         </div>
       </div>
